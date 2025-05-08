dbb1ba986afeb978cc3d5e6c83d5c1fd1898720d
OskarEriksen <omle.eriksen@gmail.com>
2025-05-08 15:13:41 +0200

    Lucide icons working, but not correct version

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,16 @@
-// import { Car, CircleUser } from "lucide-react-native";
+import { Car, CircleUser } from "lucide-react-native";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Header() {
   return (
     <View style={styles.headerContainer}>
       <View style={styles.userInfo}>
-        {/* <CircleUser color="black" size={50} strokeWidth={1} /> */}
+        <CircleUser color="black" size={50} strokeWidth={1} />
         <Text style={styles.userInfoText}>Anders Jensen</Text>
       </View>
       <View style={styles.userInfo}>
         <Text style={styles.userInfoText}>1220</Text>
-        {/* <Car color="black" size={40} strokeWidth={1} /> */}
+        <Car color="black" size={40} strokeWidth={1} />
       </View>
     </View>
   );
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
     height: 120,
     width: "100%",
-    paddingTop: 30,
+    paddingTop: 33,
     paddingLeft: 20,
     paddingRight: 20,
     flexDirection: "row",
