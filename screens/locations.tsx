3a5ba2138e00191950f25665c5448c7b522251ba
OskarEriksen <omle.eriksen@gmail.com>
2025-05-08 13:48:42 +0200

    Configure maps api keys in app.config.js file

---
+++ screens/locations.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import Header from "../components/Header";
+
+export default function Locations() {
+  return (
+    <View style={styles.screenContainer}>
+      <Header />
+      <View style={styles.locationContainer}>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    width: "100%",
+    flex: 1,
+  },
+  locationContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#000000",
+  },
+});
